d114183ff21c57c38f96520c79b4474cc952ac49
habeeb.pth <habeeb.pth@outlook.com>
2025-05-24 19:58:46 +0400

    #delete acoutning

--- Accounting/Resources/views/sections/setting-sidebar.blade.php
+++
@@ -1,6 +0,0 @@
-@if (
-    !in_array('client', user_roles()) &&
-        (in_array('accounting', user_modules())))
-<x-setting-menu-item :active="$activeMenu" menu="accounting_settings" :href="route('accounting-settings.index')"
-:text="__('accounting::app.menu.AccountingSettings')"/>
-@endif
